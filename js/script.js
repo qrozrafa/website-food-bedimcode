d729f1fb2b80675243626f708ed8579fa8dc7a66
qrozrafa <rafael_queirozs@hotmail.com>
2021-08-30 14:53:22 -0400

    scroll-top finalizado, estruturacao html add main, home, sobre, servico, cardapio, contato

--- js/script.js
+++ js/script.js
@@ -55,3 +55,11 @@ function scrollHeader(){
 window.addEventListener('scroll', scrollHeader)
 
 // SHOW SCROLL TOP
+
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top')
+    // when the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollTop.classList.add('scroll-top'); else scrollTop.classList.remove('scroll-top')
+}
+
+window.addEventListener('scroll', scrollTop)
